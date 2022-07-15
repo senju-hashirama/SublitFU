4b353c0c0ea18a71c81933265809620796ed55ea
B Monish Moger <85382114+senju-hashirama@users.noreply.github.com>
2022-07-16 00:33:50 +0530

    Add files via upload

---
+++ sublitfu.py
@@ -0,0 +1,44 @@
+from pyautogui import write
+import time
+from colorama import Fore,init
+
+init(convert=True)
+print(Fore.RED+"""
+   _____       __    ___ __     ________  __
+  / ___/__  __/ /_  / (_) /_   / ____/ / / /
+  \__ \/ / / / __ \/ / / __/  / /_  / / / / 
+ ___/ / /_/ / /_/ / / / /_   / __/ / /_/ /  
+/____/\__,_/_.___/_/_/\__/  /_/    \____/   
+
+
+""")							
+                                                                  
+print(Fore.GREEN+"""
+  You don't have to sit and type stuff in Sublit anymore!!!
+  Just copy and paste your code into sublit.txt file
+  and run sublitfu.exe.
+  Press y when you are ready, you will have 5 seconds 
+  to switch to the sublit window.
+  Once you switch click on the space where you type your code
+  Thats it!!!
+**Dont forget to delete the extra flower brackets at the end of your program**
+""")
+
+
+
+a=open("sublit.txt","r")
+
+data=a.readlines()
+while True:
+        
+    inp=input("Are you ready??y/n: ")
+    if inp=="y":
+        time.sleep(5)
+        for i in data:
+            
+            write(i.strip()+"\n")
+    elif inp=="n":
+        break
+    else:
+        print("Invalid option")
+
